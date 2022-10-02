bec415ac8a6c64f8c678d58698b26b8a02831c66
Lokie89 <traeuman@gmail.com>
2022-10-02 19:39:19 +0900

    feat : BaseballNumbers generateOnString 기능
    - Stringg 입력하여 BaseballNumbers 생성
    - 글자 길이가 다르거나, Baseball 룰에 어긋나는 숫자, 숫자가 아닌 글자를 입력할 경우 IllegalArgumentException

--- src/main/java/baseball/BaseballNumbers.java
+++ src/main/java/baseball/BaseballNumbers.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import baseball.util.StringParser;
+
 import java.util.*;
 
 /**
@@ -9,9 +11,13 @@ import java.util.*;
 public class BaseballNumbers {
 
     private final static int BASEBALL_NUMBER_CAPACITY = 3;
+    private final static int MINIMUM_NUMBER = 1;
+    private final static int MAXIMUM_NUMBER = 9;
     private final List<Integer> baseballNumbers;
 
-    private BaseballNumbers(Collection<Integer> numbers) {
+    private BaseballNumbers(Set<Integer> numbers) {
+        validateSize(numbers);
+        validateNumbers(numbers);
         this.baseballNumbers = new ArrayList<>(numbers);
     }
 
@@ -21,6 +27,43 @@ public class BaseballNumbers {
             numbers.add(BaseballRandomNumberGenerator.generate());
         }
         return new BaseballNumbers(numbers);
+    }
+
+    public static BaseballNumbers generateOnString(String s) {
+        validateLength(s);
+        List<Integer> parsedList;
+        try {
+            parsedList = StringParser.convertIntegerList(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        Set<Integer> numbers = new LinkedHashSet<>(parsedList);
+        return new BaseballNumbers(numbers);
+    }
+
+    private static void validateLength(String s) {
+        if (s.length() != BASEBALL_NUMBER_CAPACITY) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateSize(Collection<Integer> numbers) {
+        if (numbers.size() != BASEBALL_NUMBER_CAPACITY) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumbers(Set<Integer> numbers) {
+        for (Integer num : numbers) {
+            validRange(num);
+        }
+    }
+
+    private void validRange(Integer num) {
+        if (num < MINIMUM_NUMBER
+                || num > MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
--- src/test/java/baseball/BaseballNumbersTest.java
+++ src/test/java/baseball/BaseballNumbersTest.java
@@ -1,8 +1,11 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.reflect.Field;
 import java.util.HashSet;
@@ -14,7 +17,7 @@ import java.util.List;
  */
 public class BaseballNumbersTest {
 
-    @DisplayName("야구 게임 숫자의 크기가 3인지 판별")
+    @DisplayName("랜덤 야구 게임 숫자의 크기가 3인지 판별")
     @Test
     void validateNumbersSize() throws NoSuchFieldException, IllegalAccessException {
         BaseballNumbers baseballNumbers = BaseballNumbers.generateOnCapacity();
@@ -25,7 +28,7 @@ public class BaseballNumbersTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
-    @DisplayName("야구 게임 숫자가 서로 다른 숫자인지 판별")
+    @DisplayName("랜덤 야구 게임 숫자가 서로 다른 숫자인지 판별")
     @Test
     void validateUniqueNumbers() throws NoSuchFieldException, IllegalAccessException {
         BaseballNumbers baseballNumbers = BaseballNumbers.generateOnCapacity();
@@ -34,5 +37,25 @@ public class BaseballNumbersTest {
 
         List<Integer> numbers = (List<Integer>) field.get(baseballNumbers);
         assertThat(numbers.size()).isEqualTo(new HashSet<>(numbers).size());
+    }
+
+    @DisplayName("입력 받은 야구 게임 숫자 생성")
+    @ValueSource(strings = {"123", "562", "837"})
+    @ParameterizedTest
+    void generateOnString(String input) throws NoSuchFieldException, IllegalAccessException {
+        BaseballNumbers baseballNumbers = BaseballNumbers.generateOnString(input);
+        Field field = baseballNumbers.getClass().getDeclaredField("baseballNumbers");
+        field.setAccessible(true);
+
+        List<Integer> numbers = (List<Integer>) field.get(baseballNumbers);
+        assertThat(numbers.size()).isEqualTo(new HashSet<>(numbers).size());
+    }
+
+    @DisplayName("입력 받은 야구 게임 숫자 생성 Exception")
+    @ValueSource(strings = {"abc", "1234", "113", "930"})
+    @ParameterizedTest
+    void generateOnStringException(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> BaseballNumbers.generateOnString(input));
     }
 }
